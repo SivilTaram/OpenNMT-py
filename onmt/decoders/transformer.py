Fix the bert layer normalization problem.

onmt/decoders/transformer.py
@@ -64,12 +64,11 @@ class TransformerDecoderLayer(nn.Module):
                                               dropout=attention_dropout,
                                               aan_useffn=aan_useffn)
 
-        self.his_context_attn = MultiHeadedAttention(
-            heads, d_model, dropout=attention_dropout)
-        self.cur_context_attn = MultiHeadedAttention(
+        self.context_attn = MultiHeadedAttention(
             heads, d_model, dropout=attention_dropout)
         # [his, cur]
         self.his_cur_feed_forward = nn.Linear(d_model * 2, d_model)
+
         self.feed_forward = PositionwiseFeedForward(d_model, d_ff, dropout, activation)
         self.layer_norm_1 = nn.LayerNorm(d_model, eps=1e-6)
         self.layer_norm_2 = nn.LayerNorm(d_model, eps=1e-6)
@@ -95,31 +94,13 @@ class TransformerDecoderLayer(nn.Module):
             * top_his_attn ``(batch_size, T, src_len)``
             * attn_align ``(batch_size, T, src_len)`` or None
         """
-        with_align = kwargs.pop('with_align', False)
-        output, his_attns, cur_attns, his_mid, cur_mid = self._forward(*args, **kwargs)
-
-        # batch_size x T x src_len
-        top_his_attn = his_attns[:, 0, :, :].contiguous()
-        top_cur_attn = cur_attns[:, 0, :, :].contiguous()
+        output, output_m, his_mid, cur_mid = self._forward(*args, **kwargs)
 
         # batch_size x T x hidden_size
         top_his_mid = his_mid
         top_cur_mid = cur_mid
 
-        attn_align = None
-        if with_align:
-            if self.full_context_alignment:
-                # return _, (B, Q_len, K_len)
-                _, his_attns = self._forward(*args, **kwargs, future=True)
-
-            if self.alignment_heads > 0:
-                his_attns = his_attns[:, :self.alignment_heads, :, :].contiguous()
-            # layer average attention across heads, get ``(B, Q, K)``
-            # Case 1: no full_context, no align heads -> layer avg baseline
-            # Case 2: no full_context, 1 align heads -> guided align
-            # Case 3: full_context, 1 align heads -> full cte guided align
-            attn_align = his_attns.mean(dim=1)
-        return output, top_his_attn, top_cur_attn, top_his_mid, top_cur_mid, attn_align
+        return output, output_m, top_his_mid, top_cur_mid
 
     def _forward(self, inputs, history_memory_bank, current_memory_bank,
                  history_pad_mask, current_pad_mask, tgt_pad_mask,
@@ -175,28 +156,29 @@ class TransformerDecoderLayer(nn.Module):
                                       layer_cache=layer_cache, step=step)
 
         query = self.drop(query) + inputs
+
+        # M in paper
         query_norm = self.layer_norm_2(query)
 
-        his_mid, his_attns = self.his_context_attn(history_memory_bank, history_memory_bank, query_norm,
-                                                   mask=history_pad_mask,
-                                                   layer_cache=layer_cache,
-                                                   attn_type="context")
-        cur_mid, cur_attns = self.cur_context_attn(current_memory_bank, current_memory_bank, query_norm,
-                                                   mask=current_pad_mask,
-                                                   layer_cache=layer_cache,
-                                                   attn_type="context")
+        his_mid, his_attns = self.context_attn(history_memory_bank, history_memory_bank, query_norm,
+                                               mask=history_pad_mask,
+                                               layer_cache=layer_cache,
+                                               attn_type="context")
+        cur_mid, cur_attns = self.context_attn(current_memory_bank, current_memory_bank, query_norm,
+                                               mask=current_pad_mask,
+                                               layer_cache=layer_cache,
+                                               attn_type="context")
 
         mid = self.his_cur_feed_forward(torch.cat([his_mid, cur_mid], dim=2))
 
-        # the 3-rd layer norm is performed inside self.feed_forward
-        # here we use another feed forward to retain the residual connection
+        # D in paper
         output = self.feed_forward(self.drop(mid) + query)
 
-        return output, his_attns, cur_attns, his_mid, cur_mid
+        return output, query_norm, his_mid, cur_mid
 
     def update_dropout(self, dropout, attention_dropout):
         self.self_attn.update_dropout(attention_dropout)
-        self.his_context_attn.update_dropout(attention_dropout)
+        self.context_attn.update_dropout(attention_dropout)
         self.feed_forward.update_dropout(dropout)
         self.drop.p = dropout
 
@@ -269,6 +251,8 @@ class TransformerDecoder(DecoderBase):
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
 
         self.alignment_layer = alignment_layer
+        self.his_attn_layer = nn.Linear(d_model, d_model)
+        self.cur_attn_layer = nn.Linear(d_model, d_model)
 
     @classmethod
     def from_opt(cls, opt, embeddings):
@@ -393,24 +377,32 @@ class TransformerDecoder(DecoderBase):
         for i, layer in enumerate(self.transformer_layers):
             layer_cache = self.state["cache"]["layer_{}".format(i)] \
                 if step is not None else None
-            output, his_attn, cur_attn, his_mid, cur_mid, attn_align = layer(
+            output, output_m, his_mid, cur_mid = layer(
                 output,
                 history_memory_bank,
                 current_memory_bank,
                 history_pad_mask,
                 current_pad_mask,
                 tgt_pad_mask,
-                step=step,
-                with_align=with_align)
-            if attn_align is not None:
-                attn_aligns.append(attn_align)
+                step=step)
 
         output = self.layer_norm(output)
+
+        hidden_size = output_m.shape[-1]
+        his_attn = torch.matmul(output_m,
+                                self.his_attn_layer(history_memory_bank).transpose(1, 2)) * (hidden_size ** - 0.5)
+        expand_mask = history_pad_mask.long().expand_as(his_attn) * -1e10
+        his_attn = torch.softmax(his_attn + expand_mask, dim=-1)
+        cur_attn = torch.matmul(output_m,
+                                self.cur_attn_layer(current_memory_bank).transpose(1, 2)) * (hidden_size ** - 0.5)
+        expand_mask = current_pad_mask.long().expand_as(cur_attn) * -1e10
+        cur_attn = torch.softmax(cur_attn + expand_mask, dim=-1)
+
         dec_outs = output.transpose(0, 1).contiguous()
-        his_attn = his_attn.transpose(0, 1).contiguous()
-        cur_attn = cur_attn.transpose(0, 1).contiguous()
         his_mid = his_mid.transpose(0, 1).contiguous()
         cur_mid = cur_mid.transpose(0, 1).contiguous()
+        his_attn = his_attn.transpose(0, 1).contiguous()
+        cur_attn = cur_attn.transpose(0, 1).contiguous()
 
         # TODO: coverage may not work in our scenario (split attention)
         attns = {"std": his_attn}

onmt/encoders/bert.py
@@ -6,7 +6,6 @@ Implementation from: https://raw.githubusercontent.com/Zenglinxiao/OpenNMT-py/be
 import torch.nn as nn
 from onmt.encoders.transformer import TransformerEncoderLayer
 from onmt.utils.misc import sequence_mask
-import torch
 
 
 class BertEncoder(nn.Module):
@@ -39,7 +38,7 @@ class BertEncoder(nn.Module):
             [TransformerEncoderLayer(d_model, heads, d_ff,
              dropout, attention_dropout,
              max_relative_positions=max_relative_positions,
-             activation='gelu', is_bert=True) for _ in range(num_layers)])
+             activation='gelu') for _ in range(num_layers)])
 
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-12)
         self.pooler = BertPooler(d_model)
@@ -49,7 +48,7 @@ class BertEncoder(nn.Module):
         """Alternate constructor."""
         return cls(
             embeddings,
-            opt.layers,
+            opt.enc_layers,
             opt.word_vec_size,
             opt.heads,
             opt.transformer_ff,

onmt/encoders/transformer.py
@@ -35,23 +35,10 @@ class TransformerEncoderLayer(nn.Module):
         self.self_attn = MultiHeadedAttention(
             heads, d_model, dropout=attention_dropout,
             max_relative_positions=max_relative_positions)
-        self.feed_forward = PositionwiseFeedForward(d_model, d_ff, dropout,
-                                                    activation, is_bert)
+        self.feed_forward = PositionwiseFeedForward(d_model, d_ff, dropout, activation)
         self.layer_norm = nn.LayerNorm(
             d_model, eps=1e-12 if is_bert else 1e-6)
         self.dropout = nn.Dropout(dropout)
-        self.is_bert = is_bert
-
-    def residual(self, output, x):
-        """A Residual connection.
-
-        Official BERT perform residual connection on layer normed input.
-        BERT's layer_norm is done before pass into next block while onmt's
-        layer_norm is performed at the begining.
-        """
-
-        maybe_norm = self.layer_norm(x) if self.is_bert else x
-        return output + maybe_norm
 
     def forward(self, inputs, mask):
         """
@@ -64,11 +51,11 @@ class TransformerEncoderLayer(nn.Module):
 
             * outputs ``(batch_size, src_len, model_dim)``
         """
-
+        # Embedding -> [ LayerNorm -> Self Attention -> LayerNorm -> Position-wise FeedForward ]
         input_norm = self.layer_norm(inputs)
         context, _ = self.self_attn(input_norm, input_norm, input_norm,
                                     mask=mask, attn_type="self")
-        out = self.residual(self.dropout(context), inputs)
+        out = self.dropout(context) + input_norm
         return self.feed_forward(out)
 
     def update_dropout(self, dropout, attention_dropout):

onmt/modules/position_ffn.py
@@ -29,18 +29,6 @@ class PositionwiseFeedForward(nn.Module):
         self.dropout_1 = nn.Dropout(dropout)
         self.activation = get_activation_fn(activation)
         self.dropout_2 = nn.Dropout(dropout)
-        self.is_bert = is_bert
-
-    def residual(self, output, x):
-        """A Residual connection.
-
-        Official BERT perform residual connection on layer normed input.
-        BERT's layer_norm is done before pass into next block while onmt's
-        layer_norm is performed at the begining.
-        """
-
-        maybe_norm = self.layer_norm(x) if self.is_bert else x
-        return output + maybe_norm
 
     def forward(self, x):
         """Layer definition.
@@ -51,10 +39,11 @@ class PositionwiseFeedForward(nn.Module):
         Returns:
             (FloatTensor): Output ``(batch_size, input_len, model_dim)``.
         """
-
-        inter = self.dropout_1(self.activation(self.w_1(self.layer_norm(x))))
+        x_norm = self.layer_norm(x)
+        inter = self.dropout_1(self.activation(self.w_1(x_norm)))
         output = self.dropout_2(self.w_2(inter))
-        return self.residual(output, x)
+        residual_output = output + x_norm
+        return residual_output
 
     def update_dropout(self, dropout):
         self.dropout_1.p = dropout